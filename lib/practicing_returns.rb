58785bf89702c847df8689e1526490d57446320f
Rehman Ali <rehmanali001@gmail.com>
2019-07-29 20:50:50 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,11 +4,12 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
-  collection << yield(array[i])\
+  collection << yield(array[i])
   binding.pry
     yield(array[i])
     i += 1
   end
   collection
 end
+
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
